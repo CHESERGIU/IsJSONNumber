32a84dc650964410ba009c917fd523c5f8edd395
CHESERGIU <sergiu.chereches@gmail.com>
2019-05-21 08:24:52 +0300

    initial commit git

---
+++ IsNumberJSON/IsNumberJSON/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IsNumberJSON
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (IsValidJSONNumber(Console.ReadLine()) == true)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadLine();
+        }
+
+        private static bool IsValidJSONNumber(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
